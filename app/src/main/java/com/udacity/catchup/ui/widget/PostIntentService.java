9bd14ac24a9a91a92fa3a0f0c8d1c2cadf923667
barnakaracsony <git8.XyZ>
2019-12-06 11:30:14 +0100

    fix: try to fix the update bug by starting foreground service above Oreo

--- app/src/main/java/com/udacity/catchup/ui/widget/PostIntentService.java
+++ app/src/main/java/com/udacity/catchup/ui/widget/PostIntentService.java
@@ -1,31 +1,88 @@
 package com.udacity.catchup.ui.widget;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
+import com.udacity.catchup.R;
 import com.udacity.catchup.data.Repository;
 import com.udacity.catchup.data.entity.post.Post;
 import com.udacity.catchup.data.entity.subreddit.Subreddit;
 import com.udacity.catchup.util.InjectorUtils;
 
+import static androidx.core.app.NotificationCompat.PRIORITY_MIN;
+
 public class PostIntentService extends IntentService {
 
+    private static final String SERVICE_NAME = PostIntentService.class.getSimpleName();
     static final String ACTION_UPDATE_POST_WIDGET =
             "com.udacity.catchup.action.update_post_widget";
 
     public PostIntentService() {
-        super("PostIntentService");
+        super(SERVICE_NAME);
     }
 
     static void startActionUpdateWidget(Context context) {
         Intent intent = new Intent(context, PostIntentService.class);
         intent.setAction(ACTION_UPDATE_POST_WIDGET);
-        context.startService(intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent);
+        } else {
+            context.startService(intent);
+        }
+        Log.d(PostIntentService.class.getSimpleName(), "trying to start service");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground();
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void startForeground() {
+        String channelId = createNotificationChannel();
+        if (channelId == null) {
+            return;
+        }
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(PRIORITY_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(1, notification);
+    }
+
+    @Nullable
+    @RequiresApi(Build.VERSION_CODES.O)
+    private String createNotificationChannel() {
+        NotificationChannel channel =
+                new NotificationChannel(
+                        PostIntentService.SERVICE_NAME,
+                        PostIntentService.SERVICE_NAME,
+                        NotificationManager.IMPORTANCE_NONE);
+        NotificationManager service =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (service != null) {
+            service.createNotificationChannel(channel);
+            return PostIntentService.SERVICE_NAME;
+        }
+        return null;
     }
 
     @Override
